cedb522b18a81d1cb4ba98013b119c6fd346cdcf
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-11-02 16:55:36 +0100

    Update Constructors.py

--- pages/Constructors.py
+++ pages/Constructors.py
@@ -95,16 +95,15 @@ fig = st.bar_chart(condf1, y='points_sum', x='constructors_name_')
 condf2 = condf1[condf1.constructors_name_ == 'Ferrari']
 condf2 = condf2.drop(1085)
 condf2.sort_values(by='year_', ascending=True)
+
 with st.echo(code_location='below'):
     import plotly.express as px
+fig = px.scatter(x=condf2["year_"], y=condf2["points_sum"],trendline="ols", trendline_scope="overall", trendline_color_override="black")
+ st.plotly_chart(fig)
+fig.update_layout(
+xaxis_title="Seizoen",
+yaxis_title="Aantal punten")
 
-       fig = px.scatter(x=condf2["year_"], y=condf2["points_sum"],trendline="ols", trendline_scope="overall", trendline_color_override="black")
-       st.plotly_chart(fig)
-       fig.update_layout(
-        xaxis_title="Seizoen",
-        yaxis_title="Aantal punten",
-       )
-
-       st.write(fig)
+st.write(fig)
 
 
