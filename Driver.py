a79d04ab1bc1712a37ab6d3372e230bd6f6e1827
EOlivierr <114393619+EOlivierr@users.noreply.github.com>
2022-11-02 20:42:17 +0100

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -100,17 +100,17 @@ st.plotly_chart(fig3)
 
 #Nationality plot nieuw
 driversdf3 = drivers.copy()
-driversdf3['driver_name'] = driversdf3['forename'] + ' ' + driversdf3['surname']
-driversdf3 = driversdf3.drop(columns =['forename', 'surname'])
+#driversdf3['driver_name'] = driversdf3['forename'] + ' ' + driversdf3['surname']
+#driversdf3 = driversdf3.drop(columns =['forename', 'surname'])
 
-era_df= merged_df[['driver_name', 'driver_nationality']]
+#era_df= merged_df[['driver_name', 'driver_nationality']]
 #era_df =era_df.rename(columns = {'driver_name':'naam', 'driver_nationality':'herkomst'})
 #driversdf4 =driversdf3.rename(columns = {'driver_name':'naam', 'year':'jaar'})
-era_df2= era_df.merge(driversdf3, on='driver_name')
+#era_df2= era_df.merge(driversdf3, on='driver_name')
 
-st.dataframe(era_df)
+#st.dataframe(era_df)
 st.dataframe(driversdf3)
-
+#st.dataframe(driversdf3)
 
 
 
