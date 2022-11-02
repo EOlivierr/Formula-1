2cd38d8415f207038da5a44dd23c1a6d925286ef
EOlivierr <114393619+EOlivierr@users.noreply.github.com>
2022-11-02 12:28:32 +0100

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -120,23 +120,23 @@ with st.echo(code_location='below'):
 
     fig = go.Figure()
 
-teller = 0
-buttonlist = [dict(label = "Kies een coureur", method='update', args=[{"visible": [True*len(alle_drivers)]}])]
+    teller = 0
+    buttonlist = [dict(label = "Kies een coureur", method='update', args=[{"visible": [True*len(alle_drivers)]}])]
 
-for i in alle_drivers:
-    df2= scat_df[scat_df['driver_name'] == i]
+    for i in alle_drivers:
+        df2= scat_df[scat_df['driver_name'] == i]
     
-    fig.add_trace(go.Scatter(x=df2["grid"], y=df2["position"], mode='markers', name=str(i)))
+        fig.add_trace(go.Scatter(x=df2["grid"], y=df2["position"], mode='markers', name=str(i)))
     
-    lijst = [False]*len(alle_drivers)
-    lijst[teller] = True
-    teller = teller + 1
+        lijst = [False]*len(alle_drivers)
+        lijst[teller] = True
+        teller = teller + 1
     
-    one_button = dict(label = str(i), method='update', args=[{"visible": lijst}])
-    buttonlist.append(one_button)
+        one_button = dict(label = str(i), method='update', args=[{"visible": lijst}])
+        buttonlist.append(one_button)
     
-fig.update_layout(
-updatemenus=[
+    fig.update_layout(
+    updatemenus=[
         dict(
             buttons=buttonlist,
             direction="down",
@@ -150,12 +150,12 @@ updatemenus=[
     ]
 )
 
-fig.update_layout(title='Correlatie tussen Qualificatie positie en eindpositie',
+    fig.update_layout(title='Correlatie tussen Qualificatie positie en eindpositie',
                    xaxis_title='Qualificatie positie',
                    yaxis_title='Eindpositie',
                    template = "plotly_dark")
 
-fig.update_yaxes(type='linear')
+    fig.update_yaxes(type='linear')
 
 
 
