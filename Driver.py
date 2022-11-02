2906bb099f89ef436b5549db921ff0e4e5b6bf2d
EOlivierr <114393619+EOlivierr@users.noreply.github.com>
2022-11-02 21:08:25 +0100

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -106,7 +106,7 @@ start_date5 = min(nat_df1['year'])
 end_date5 = max(nat_df1['year'])
 max_days5 = end_date5-start_date5
 slider5 = st.slider('Selecteer de datum', min_value=start_date5 ,max_value=end_date5)
-#st.markdown(slider5)
+st.markdown(slider5)
 
 driver_country3 = nat_df1.groupby('nationality').driver_name.nunique().reset_index() 
 driver_country3 = driver_country3.rename(columns = {'driver_name': 'driver_counts'})
