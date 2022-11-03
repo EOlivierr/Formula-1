2322f3d086f1f8a875ad54bdfd0af628936a51e0
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-11-03 14:10:30 +0100

    Update API.py

--- pages/API.py
+++ pages/API.py
@@ -9,7 +9,7 @@ import streamlit as st
 import plotly.figure_factory as ff
 import requests
 
-url = http://ergast.com/api/f1/driverStandings/1
+url = "http://ergast.com/api/f1/driverStandings/1"
 
 
 response = requests.request("GET", url = url)
