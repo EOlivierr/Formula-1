24e4992dcb9c6b33ec22ae2abb4e2cf6a5fa696a
EOlivierr <114393619+EOlivierr@users.noreply.github.com>
2022-11-03 16:27:39 +0100

    Update Circuits.py

--- pages/Circuits.py
+++ pages/Circuits.py
@@ -7,6 +7,7 @@ import numpy as np
 import plotly.graph_objects as go
 import streamlit as st
 import plotly.figure_factory as ff
+from PIL import Image
 PAGE_NAME = "Circuits"
 st.set_page_config(layout="wide")
 
