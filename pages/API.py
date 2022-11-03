e64bbc5d02235c4b9211ecacd71bd4b60f16b741
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-11-03 14:10:09 +0100

    Update and rename API to API.py

--- pages/API.py
+++ pages/API.py
@@ -8,3 +8,9 @@ import plotly.graph_objects as go
 import streamlit as st
 import plotly.figure_factory as ff
 import requests
+
+url = http://ergast.com/api/f1/driverStandings/1
+
+
+response = requests.request("GET", url = url)
+
