3b2f6e4077d87b67aeb77c380917152b66da0ed7
EOlivierr <114393619+EOlivierr@users.noreply.github.com>
2022-11-03 23:58:03 +0100

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -29,7 +29,7 @@ response = requests.request("GET", url, headers=headers)
 
 json=response.json()
 index = pd.Index(['s1', 's2', 's3', 's1', 's2', 's3', 's1', 's2', 's3', 's1'])
-df=pd.DataFrame(json).set_index('id', drop=True, inplace=True)
+df=pd.DataFrame(json).set_index('id', drop=False, inplace=False)
 df1=df.sample()
 
 with col4:
