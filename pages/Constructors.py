6704fa1789a511fdcadd5afcd2e76122c6d7915d
EOlivierr <114393619+EOlivierr@users.noreply.github.com>
2022-11-04 06:37:50 +0100

    Update Constructors.py

--- pages/Constructors.py
+++ pages/Constructors.py
@@ -19,21 +19,23 @@ col1, col2, col3, col4 = st.columns([1, 3, 1, 1])
 
 image = Image.open('formula-1-logo-5-3.png')
 
-#url = 'https://f1-drivers-quotes.p.rapidapi.com/quotes'
+url = 'https://f1-drivers-quotes.p.rapidapi.com/quotes'
 
-#headers = {
-	#"X-RapidAPI-Key": "db7edfb24amshfb89605c2d68e9bp18a964jsn86117363852a",
-	#"X-RapidAPI-Host": "f1-drivers-quotes.p.rapidapi.com"
-#}
-#response = requests.request("GET", url, headers=headers)
+headers = {
+	"X-RapidAPI-Key": "db7edfb24amshfb89605c2d68e9bp18a964jsn86117363852a",
+	"X-RapidAPI-Host": "f1-drivers-quotes.p.rapidapi.com"
+}
+response = requests.request("GET", url, headers=headers)
 
-#json=response.json()
-#df=pd.DataFrame(json).set_index(inplace=True)
-#df1=df.sample()
+data=response.json()
+df1 = pd.DataFrame(data, index = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
+df1=df1.sample()
+#st.dataframe(df1)
+
 
 with col4:
 	st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-	#st.write(df1['author'], df1['quote'])	
+	st.write(df1['author'], df1['quote'])		
 
 circuits= pd.read_csv('circuits.csv')
 constructor_results= pd.read_csv('constructor_results.csv')
